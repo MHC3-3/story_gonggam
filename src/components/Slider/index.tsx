94d0c707997728e2479a69b3a1babee088664fc9
Hyohyun Nam <skagygus8@gmail.com>
2022-09-22 17:55:25 +0900

    feat: 슬라이드

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import { Carousel } from 'react-responsive-carousel';
+import styles from './slider.module.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 interface Props {
   imgs: string[];
@@ -8,15 +10,17 @@ interface Props {
 
 const Slider = ({ imgs, alt }: Props) => {
   return (
-    <Carousel>
-      {imgs.map((url, idx) => {
-        return (
-          <div key={`slider-item-${url}`}>
-            <Image src={url} alt={alt + '' + idx} layout='fill' />
-          </div>
-        );
-      })}
-    </Carousel>
+    <div className={styles.slider}>
+      <Carousel showStatus={false} autoPlay>
+        {imgs.map((url, idx) => {
+          return (
+            <div key={`slider-item-${url}`}>
+              <Image src={url} alt={alt + '' + idx} width={358} height={224} />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Slider from '@/components/Slider';
 import SubTitle from '@/components/SubTitle';
 // import { getTest } from 'apis/test';
 import type { NextPage } from 'next';
@@ -18,7 +19,18 @@ const Home: NextPage = () => {
   // }, []);
 
   const sectionClick = () => {};
-
+  const cafeArray = [
+    '/pngs/cafe/cafe1.png',
+    '/pngs/cafe/cafe2.png',
+    '/pngs/cafe/cafe3.png',
+    '/pngs/cafe/cafe4.png',
+  ];
+  const bariArray = [
+    '/pngs/barista/barista1.png',
+    '/pngs/barista/barista2.png',
+    '/pngs/barista/barista3.png',
+    '/pngs/barista/barista4.png',
+  ];
   return (
     <div>
       <Head>
@@ -52,6 +64,7 @@ const Home: NextPage = () => {
             다과가 준비되어있습니다.'
             onClick={sectionClick}
           />
+          <Slider imgs={cafeArray} alt='cafe' />
 
           {/* <div>
             <Image src='/pngs/cafe/cafe1.png' alt='cafe1' layout='fill' />
@@ -59,6 +72,32 @@ const Home: NextPage = () => {
             <Image src='/pngs/cafe/cafe3.png' alt='cafe3' layout='fill' />
             <Image src='/pngs/cafe/cafe4.png' alt='cafe4' layout='fill' />
           </div> */}
+        </section>
+        <section>
+          <SubTitle
+            floor='2층'
+            title='생활문화지원센터'
+            subTitle='생활문화 프로그램은 누구나 신청 가능하며 난타, 기타교실, 라틴댄스, 댄스로빅, 요가 등 다양한 강습과 체험프로그램을 진행하고 있습니다.'
+            onClick={sectionClick}
+          />
+        </section>
+        <section>
+          <SubTitle
+            floor='3층'
+            title='바리스타 교육장'
+            subTitle='바리스타 교육과 동시에 자격검정(자격취득)이 한 곳에서 이루어져 교육생들의 만족도를 높이고 전문 기술 습득과 일자리 창출의 기회가 되어 꾸준한 수요를 창출하고 있습니다.'
+            onClick={sectionClick}
+          />
+          <Slider imgs={bariArray} alt='barista' />
+        </section>
+        <section>
+          <h2>찾아오시는길</h2>
+          <dl>
+            <dt>이용시간</dt>
+            <dd>10:00~18:00 (일요일은 휴무입니다.)</dd>
+            <dt>연락처</dt>
+            <dd>02-2686-5952</dd>
+          </dl>
         </section>
       </main>
     </div>
