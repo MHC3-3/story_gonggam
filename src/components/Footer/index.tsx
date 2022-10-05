c1c4c62cfcd150136f4b61700b24d9424353f9e1
sungyeon <csy357@gmail.com>
2022-10-05 22:17:02 +0900

    fix: 푸터, 결과페이지 행간, 도토리 파비콘

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './footer.module.scss';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
@@ -9,6 +10,9 @@ const Footer = () => {
         <span>서울특별시 구로구 오류로8길 51 스토리공감</span>
       </h3>
       <p>Made by. 버들시삼토리팀</p>
+      <div className={styles.footerImg}>
+        <Image src='/pngs/threeShot.png' alt='threeShot' width={82} height={32} />
+      </div>
     </footer>
   );
 };
