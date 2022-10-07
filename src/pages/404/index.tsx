18c67b553af72e582fb0f7c3873fffd9d8a78b0e
Hyohyun Nam <skagygus8@gmail.com>
2022-10-07 21:47:20 +0900

    feat: error 페이지 추가

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from './error.module.scss';
 
 const error = () => {
-  return <div className={styles.error}>잘못된경로입니다</div>;
+  return (
+    <div className={styles.error}>
+      <h1>404</h1>
+      <h2>잘못된 경로 입니다.</h2>
+      <Image src={'/pngs/tory.png'} alt='error' width={248} height={248} />
+      <Link href={'/main'}>
+        <a className={styles.link}>홈페이지로 돌아가기</a>
+      </Link>
+    </div>
+  );
 };
 
 export default error;
